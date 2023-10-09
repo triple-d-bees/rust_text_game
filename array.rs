40ccb153df7ac4dc7e71bb8127c2f22b5d1eaf20
DDD <ddd@localhost>
2023-10-09 16:56:34 -0500

    moving map

--- array.rs
+++ array.rs
@@ -38,7 +38,7 @@ fn main(){
     map.state[1][10] = "1".to_string();
     map.state[9][10] = "9".to_string();
     init_map(&mut map, width, height);
-    print_map(&map);
+    print_map(&map, &hero);
     println!("Please type a direction - N, E, S, W,");
     println!(" or x to escape:");
     let mut input_string = String::new();
@@ -51,7 +51,7 @@ fn main(){
         remove_old(hero.x,hero.y, &mut map);
         move_character(&mut hero, &ip);
         update_place(&hero, &mut map);
-        print_map(&map);
+        print_map(&map, &hero);
         // println!("You wrote {}", &ip);
 
 
@@ -61,7 +61,6 @@ fn main(){
 }
 fn move_character(h: &mut Hero, s: &String){
     println!("Your hero was at {}, {}", h.x.to_string(),h.y.to_string());
-   
     if s == "N"{
         move_character_north(h);
         println!("Moving {}!",s);
@@ -82,25 +81,26 @@ fn move_character(h: &mut Hero, s: &String){
         println!("Where are you trying to go?\nDirections are N, E, S, or W");
         println!("x or q to quit");
     }
+
     
 }
 fn move_character_north(h: &mut Hero){
-    if h.y > 1{
+    if h.y > 2{
         h.y -=1;
     }
 }
 fn move_character_south(h: &mut Hero){
-    if h.y < 1000{
+    if h.y < 999{
         h.y +=1;
     }
 }
 fn move_character_east(h: &mut Hero){
-    if h.x < 1000{
+    if h.x < 999{
         h.x +=1;
     }
 }
 fn move_character_west(h: &mut Hero){
-    if h.x >1{
+    if h.x >2{
         h.x -=1;
     }
 }
@@ -113,34 +113,58 @@ fn init_map(m: &mut Map, width: usize, height: usize){
             if o == 0 {
                 m.state[n][o] = "|".to_string();
             }
+            else if o == 1000{
+                m.state[n][o] = "|".to_string();
+            }
             else if n == 0 {
+                m.state[n][o] = "-".to_string();
+            }
+            else if n == 1000 {
                 m.state[n][o] = "-".to_string();
             }
             else if o == 1  {
                 m.state[n][o] = n.to_string();
             }
             else {
-                m.state[n][o] = "0".to_string();
-            }
-           
+                m.state[n][o] = ".".to_string();
+            }     
         }
     }
-
 }
 
 fn remove_old(x: i32, y: i32, m: &mut Map){
-    m.state[y as usize][x as usize] = "0".to_string();
+    m.state[y as usize][x as usize] = ",".to_string();
 }
 fn update_place(h :&Hero, m: &mut Map){
     m.state[h.y as usize][h.x as usize] = h.icon.to_string();
 }
 
-fn print_map(m: &Map){
-    for n in 0..10{
-        for o in 0..50{
+fn print_map(m: &Map, h :&Hero){
+    let mut base_x = 0;
+    let mut base_y = 0;
+    let mut max_x = 50;
+    let mut max_y = 10;
+    if h.y > 10 {
+        base_y = (h.y - 5) as usize;
+        max_y = (h.y + 5) as usize;
+    }
+    else if h.y > 989 {
+        base_y = 990 as usize;
+        max_y = 1000 as usize;
+    }
+    if h.x > 30 {
+        base_x = (h.x - 24) as usize;
+        max_x = (h.x + 24) as usize;
+    }
+    else if h.x > 989 {
+        base_x = 990 as usize;
+        max_x = 1000 as usize;
+    }
+    for n in base_y..=max_y{
+        for o in base_x..=max_x{
             print!("{}", m.state[n][o]);
         }
-        println!("");
+        println!("> Hero is at {}, {}", h.x.to_string(),h.y.to_string());
     }
 
 }
